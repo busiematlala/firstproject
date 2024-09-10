0d392c1138e1f897553854b4791b400f5feb50c9
busie <busie@MatebookD15>
2024-09-10 14:12:25 +0200

    created a application that adds two numbers

--- src/main/java/com/mycompany/githubpush/Githubpush.java
+++ src/main/java/com/mycompany/githubpush/Githubpush.java
@@ -10,7 +10,12 @@ package com.mycompany.githubpush;
  */
 public class Githubpush {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args){
+        
+       int a = 5;
+       int b = 10;
+       int c = a + b;
+        System.out.println(c);
+        
     }
 }
